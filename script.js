2db60978d31f5c54ef7a05e010eeee56deeb9f08
rasyafirdausrks-maker <rasyafirdausrks@gmail.com>
2026-06-23 23:16:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Portfolio berhasil dimuat");
+function updateClock() {
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+
+    document.getElementById("clock").textContent =
+        ${hours}:${minutes}:${seconds};
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
